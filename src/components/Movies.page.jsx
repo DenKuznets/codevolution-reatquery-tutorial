4dfc45a735fe539d2b1562bb8c68360fc203f0f0
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-07-10 16:36:41 +0300

    traditional data fetching setup

--- src/components/Movies.page.jsx
+++ src/components/Movies.page.jsx
@@ -1,8 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const MoviesPage = () => {
-  return (
-    <div>Movies page</div>
-  )
-}
+    const [isLoading, setIsLoading] = useState(true);
+    const [data, setData] = useState([]);
 
-export default MoviesPage
+    useEffect(() => {
+        axios.get("http://localhost:4000/movies").then((res) => {
+            setData(res.data);
+            setIsLoading(false);
+        });
+    }, []);
+
+    if (isLoading) {
+        return <h2>Loading...</h2>;
+    }
+
+    return (
+        <>
+            <h2>Movies Page</h2>
+            {data.map((movie, index) => {
+                return <div key={index}>{movie.title}</div>;
+            })}
+        </>
+    );
+};
+
+export default MoviesPage;
